953305ff471f1a801301268d6cc10a4284e34e09
Uyen <uyen.nguyenminhduy@gmail.com>
2022-08-07 11:20:06 +0700

    Initial Commit

---
+++ RMIT-Dice2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RMIT-Dice2
+//
+//  Created by Uyen Nguyen Minh Duy on 07/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RMIT-Dice2/RMIT_Dice2App.swift
@@ -0,0 +1,17 @@
+//
+//  RMIT_Dice2App.swift
+//  RMIT-Dice2
+//
+//  Created by Uyen Nguyen Minh Duy on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct RMIT_Dice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
